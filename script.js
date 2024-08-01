bc25d4890923e0c1ba269cfc048ebcd13cf6b799
Jin Nakano <jin.nakano.canada@gmail.com>
2024-07-31 20:24:38 -0700

    finished

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
 $(function() {
   // your code here
+  const accordionHeader = $('.accordion-header')
+  const accordionContent = $('.accordion-content')
+
+  accordionHeader.on('click', function() {
+    accordionContent.not($(this).next()).slideUp()
+    $(this).next().slideToggle()
+  })
+
+  const toDos = $('.todos')
+
+  function getData() {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: `https://dummyjson.com/todos`,
+        type: `GET`,
+        success: function(response) {
+          let toDoList = ''
+          response.todos.forEach(function(x) {
+            toDoList += `<li>${x.todo}</li>`
+          });
+          resolve(toDoList)
+        },
+        error: function(error) {
+          reject(error)
+        }
+      })
+    })
+  }
+
+  toDos.on('click', async function(){
+    const data = await getData()
+    toDos.find('ul').append(data)
+  })
 })
+
